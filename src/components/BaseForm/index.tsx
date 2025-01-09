70267694d2cccd112a557d5f6887beaf63fee4aa
KhalidO121 <khalidokewole383@gmail.com>
2025-01-09 17:25:35 +0000

    Setting up Base form for all the forms in the wizard.

--- src/components/BaseForm/index.tsx
+++ src/components/BaseForm/index.tsx
@@ -1,5 +1,23 @@
-const BaseForm = () => {
-  return <form></form>;
+import { ReactNode } from "react";
+
+const BaseForm = ({
+  children,
+  formTitle,
+  buttonTitle,
+}: {
+  children: ReactNode;
+  formTitle: string;
+  buttonTitle: string;
+}) => {
+  return (
+    <form className="h-auto w-[25vw] p-6 rounded-md">
+      <h1 className="text-2xl font-bold font-roboto">{formTitle}</h1>
+      <div className="flex flex-col w-full gap-4 my-5">{children}</div>
+      <button className="h-auto w-full bg-blue-500 p-2 rounded-md">
+        <p className="text-white font-roboto text-sm">{buttonTitle}</p>
+      </button>
+    </form>
+  );
 };
 
 export default BaseForm;
--- src/components/Fields/TextField/index.tsx
+++ src/components/Fields/TextField/index.tsx
@@ -1,6 +1,9 @@
-const TextField = () => {
+const TextField = ({ title }: { title: string }) => {
   return (
-    <div className="border-gray-300 p-3 font-roboto text-sm placeholder:font-roboto placeholder:text-sm border rounded-md h-10 w-full"></div>
+    <div className="flex flex-col gap-2">
+      <p className="text-sm text-gray-600">{title}</p>
+      <input className="border-gray-300 p-3 font-roboto text-sm placeholder:font-roboto placeholder:text-sm border rounded-md h-10 w-full" />
+    </div>
   );
 };
 
